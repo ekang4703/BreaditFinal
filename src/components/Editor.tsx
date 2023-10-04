ef1759d4611f0f7e824a9d6bc6ca71536aa692b9
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-10-04 04:10:14 -0700

    Update Editor.tsx

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -192,7 +192,7 @@ export const Editor: React.FC<EditorProps> = ({ subredditId }) => {
           }
         });
         let searchResponse = response.data;
-        console.log(searchresponse)
+        console.log(searchResponse)
 
         /*
         if (!searchResponse.ok) {
