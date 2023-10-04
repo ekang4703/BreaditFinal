a351e4a86d4ff371f6b68d8904a6986836d62934
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-10-03 23:59:11 -0700

    Update search.ts

--- src/app/api/LLM/search.ts
+++ src/app/api/LLM/search.ts
@@ -1,50 +1,43 @@
 import { supabaseAdmin } from "@/utils";
 
 export const config = {
-  runtime: "edge"
+    runtime: "edge",
 };
 
 const handler = async (req: Request): Promise<Response> => {
-  try {
-    const { query, apiKey, matches } = (await req.json()) as {
-      query: string;
-      apiKey: string;
-      matches: number;
-    };
+    try {
+        const { query, matches } = (await req.json()) as { query: string, matches: number};
 
-    const input = query.replace(/\n/g, " ");
+        const response = await fetch("https://api.openai.com/v1/embeddings", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${process.env.OPENAI_API_KEY!}`
+            },
+            body: JSON.stringify({
+                model: "text-embedding-ada-002",
+                input: query
+            })
+        });
 
-    const res = await fetch("https://api.openai.com/v1/embeddings", {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${apiKey}`
-      },
-      method: "POST",
-      body: JSON.stringify({
-        model: "text-embedding-ada-002",
-        input
-      })
-    });
+        const json = await response.json();
+        const embedding = json.data[0].embedding;
 
-    const json = await res.json();
-    const embedding = json.data[0].embedding;
+        const { data: chunks, error } = await supabaseAdmin.rpc("tool_data_search", {
+            query_embedding: embedding,
+            similarity_threshold: 0.01,
+            match_count: matches
+        });
 
-    const { data: chunks, error } = await supabaseAdmin.rpc("tool_data_search", {
-      query_embedding: embedding,
-      similarity_threshold: 0.01,
-      match_count: matches
-    });
+        if (error) {
+            console.log(error)
+            return new Response("Error", { status: 500 });
+        }
 
-    if (error) {
-      console.error(error);
-      return new Response("Error", { status: 500 });
+        return new Response(JSON.stringify(chunks), { status: 200 });
+    } catch (e) {
+        return new Response("Error", { status: 500 });
     }
-
-    return new Response(JSON.stringify(chunks), { status: 200 });
-  } catch (error) {
-    console.error(error);
-    return new Response("Error", { status: 500 });
-  }
 };
 
 export default handler;
