32f78a9c73c8e17670281cc1e2cc8b4a03a1c052
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-10-04 00:36:44 -0700

    Update search.ts

--- src/app/api/LLM/search.ts
+++ src/app/api/LLM/search.ts
@@ -4,39 +4,41 @@ export const config = {
     runtime: "edge",
 };
 
-const handler = async POST(req: Request): Promise<Response> => {
-    try {
-        const { query, matches } = (await req.json()) as { query: string, matches: number};
-
-        const response = await fetch("https://api.openai.com/v1/embeddings", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${process.env.OPENAI_API_KEY!}`
-            },
-            body: JSON.stringify({
-                model: "text-embedding-ada-002",
-                input: query
-            })
-        });
-
-        const json = await response.json();
-        const embedding = json.data[0].embedding;
-
-        const { data: chunks, error } = await supabaseAdmin.rpc("tool_data_search", {
-            query_embedding: embedding,
-            similarity_threshold: 0.01,
-            match_count: matches
-        });
-
-        if (error) {
-            console.log(error)
+const handler = async (req: Request): Promise<Response> => {
+    if (req.method === 'POST') {
+        try {
+            const { query, matches } = (await req.json()) as { query: string, matches: number};
+    
+            const response = await fetch("https://api.openai.com/v1/embeddings", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${process.env.OPENAI_API_KEY!}`
+                },
+                body: JSON.stringify({
+                    model: "text-embedding-ada-002",
+                    input: query
+                })
+            });
+    
+            const json = await response.json();
+            const embedding = json.data[0].embedding;
+    
+            const { data: chunks, error } = await supabaseAdmin.rpc("tool_data_search", {
+                query_embedding: embedding,
+                similarity_threshold: 0.01,
+                match_count: matches
+            });
+    
+            if (error) {
+                console.log(error)
+                return new Response("Error", { status: 500 });
+            }
+    
+            return new Response(JSON.stringify(chunks), { status: 200 });
+        } catch (e) {
             return new Response("Error", { status: 500 });
         }
-
-        return new Response(JSON.stringify(chunks), { status: 200 });
-    } catch (e) {
-        return new Response("Error", { status: 500 });
     }
 };
 
