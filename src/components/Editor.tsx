f964d6f666fd15b758c5afb0bec2be02c3ca6cb6
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-10-04 00:05:31 -0700

    Update Editor.tsx

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -186,7 +186,7 @@ export const Editor: React.FC<EditorProps> = ({ subredditId }) => {
     
         setLoading(true);
     
-        const searchResponse = await fetch(`/src/app/api/LLM/search`, {
+        const searchResponse = await fetch(`/src/app/api/LLM/search.ts`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json"
