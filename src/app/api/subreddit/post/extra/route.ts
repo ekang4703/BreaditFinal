ab8f8b4a369b9251533fb2ac0c5415d719e29a1c
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-10-04 04:02:06 -0700

    Update route.ts

--- src/app/api/subreddit/post/extra/route.ts
+++ src/app/api/subreddit/post/extra/route.ts
@@ -1,43 +1,37 @@
 import { supabaseAdmin } from "@/utils";
-import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        if (req.method !== "POST") {
-            return res.status(405).json({ error: "Method Not Allowed" });
-        }
+export async function POST(req: Request) {
+  try {
+    const { query, matches } = (await req.json()) as { query: string, matches: number};
 
-        const { query, matches } = req.body as { query: string, matches: number };
+    const response = await fetch("https://api.openai.com/v1/embeddings", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY!}`,
+      },
+      body: JSON.stringify({
+        model: "text-embedding-ada-002",
+        input: query,
+      }),
+    });
 
-        const response = await fetch("https://api.openai.com/v1/embeddings", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
-            },
-            body: JSON.stringify({
-                model: "text-embedding-ada-002",
-                input: query
-            })
-        });
+    const json = await response.json();
+    const embedding = json.data[0].embedding;
 
-        const json = await response.json();
-        const embedding = json.data[0].embedding;
+    const { data: chunks, error } = await supabaseAdmin.rpc("tool_data_search", {
+      query_embedding: embedding,
+      similarity_threshold: 0.01,
+      match_count: matches,
+    });
 
-        const { data: chunks, error } = await supabaseAdmin.rpc("tool_data_search", {
-            query_embedding: embedding,
-            similarity_threshold: 0.01,
-            match_count: matches
-        });
-
-        if (error) {
-            console.error(error);
-            return res.status(500).json({ error: "Internal Server Error" });
-        }
-
-        return res.status(200).json(chunks);
-    } catch (e) {
-        console.error(e);
-        return res.status(500).json({ error: "Internal Server Error" });
+    if (error) {
+      console.log(error);
+      return new Response("Error", { status: 500 });
     }
-}
+
+    return new Response(JSON.stringify(chunks), { status: 200 });
+  } catch (e) {
+    return new Response("Error", { status: 500 });
+  }
+};
