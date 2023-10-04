5c17d21e0af38624062e5fe0e410cbd4cdf68e21
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-10-04 04:33:54 -0700

    Update route.ts

--- src/app/api/subreddit/post/extraAnswer/route.ts
+++ src/app/api/subreddit/post/extraAnswer/route.ts
@@ -1,37 +1,13 @@
-import { supabaseAdmin } from "@/utils";
+import { OpenAIStream } from "@/utils";
 
 export async function POST(req: Request) {
-  try {
-    const { query, matches } = (await req.json()) as { query: string, matches: number};
+    try {
+        const { prompt } = (await req.json()) as { prompt: string };
 
-    const response = await fetch("https://api.openai.com/v1/embeddings", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY!}`,
-      },
-      body: JSON.stringify({
-        model: "text-embedding-ada-002",
-        input: query,
-      }),
-    });
-
-    const json = await response.json();
-    const embedding = json.data[0].embedding;
-
-    const { data: chunks, error } = await supabaseAdmin.rpc("tool_data_search", {
-      query_embedding: embedding,
-      similarity_threshold: 0.01,
-      match_count: matches,
-    });
-
-    if (error) {
-      console.log(error);
-      return new Response("Error", { status: 500 });
+        const stream = await OpenAIStream(prompt);
+        
+        return new Response(stream);
+    } catch (e) {
+        return new Response("Error #2", { status: 500 });
     }
-
-    return new Response(JSON.stringify(chunks), { status: 200 });
-  } catch (e) {
-    return new Response("Error Evol", { status: 500 });
-  }
 };
