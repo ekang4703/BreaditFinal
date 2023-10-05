515a52f4f3499b252dbc8f5c5bd4c6740b620e15
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-10-05 14:41:39 -0700

    Update PostComment.tsx

--- src/components/comments/PostComment.tsx
+++ src/components/comments/PostComment.tsx
@@ -65,6 +65,7 @@ const PostComment: FC<PostCommentProps> = ({
     onSuccess: () => {
       router.refresh()
       setIsReplying(false)
+      let formattedText = `<div style="color: red;">${comment.text}</div>`;
     },
   })
 
@@ -87,7 +88,7 @@ const PostComment: FC<PostCommentProps> = ({
         </div>
       </div>
 
-      <p className='text-sm text-zinc-900 mt-2'>{comment.text}</p>
+      <p className='text-sm text-zinc-900 mt-2' dangerouslySetInnerHTML={{ __html: formattedText }} />
 
       <div className='flex gap-2 items-center'>
         <CommentVotes
