fa94fa73f1e0a487b4add0a25e183b17c6399366
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-10-05 15:59:56 -0700

    Update Editor.tsx

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -63,7 +63,7 @@ export const Editor: React.FC<EditorProps> = ({ subredditId }) => {
 
   const [showSettings, setShowSettings] = useState<boolean>(false);
   const [mode, setMode] = useState<"search" | "chat">("chat");
-  const [matchCount, setMatchCount] = useState<number>(5);
+  const [matchCount, setMatchCount] = useState<number>(3);
   const [apiKey, setApiKey] = useState<string>(process.env.OPENAI_API_KEY!);
 
   const { mutate: createPost } = useMutation({
