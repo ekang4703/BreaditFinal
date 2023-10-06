e5be9ea63a83bb5f50f51df88b0d2db91e7c9d3b
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-10-06 00:17:01 -0700

    Update page.tsx

--- src/app/r/[slug]/submit/page.tsx
+++ src/app/r/[slug]/submit/page.tsx
@@ -1,51 +1,47 @@
-import React, { useState } from 'react';
-import { Editor } from '@/components/Editor';
-import { Button } from '@/components/ui/Button';
-import { db } from '@/lib/db';
-import { notFound } from 'next/navigation';
+import { Editor } from '@/components/Editor'
+import { Button } from '@/components/ui/Button'
+import { db } from '@/lib/db'
+import { notFound } from 'next/navigation'
 
-const Page = ({ params }: pageProps) => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [subreddit, setSubreddit] = useState(null);
+interface pageProps {
+  params: {
+    slug: string
+  }
+}
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const subredditData = await db.subreddit.findFirst({
-          where: {
-            name: params.slug,
-          },
-        });
+const page = async ({ params }: pageProps) => {
+  const subreddit = await db.subreddit.findFirst({
+    where: {
+      name: params.slug,
+    },
+  })
 
-        if (subredditData) {
-          setSubreddit(subredditData);
-        } else {
-          notFound();
-        }
-      } catch (error) {
-        console.error('Error fetching data: ', error);
-      } finally {
-        setIsLoading(false); // Set loading state to false when data fetching is complete
-      }
-    };
-
-    fetchData();
-  }, [params.slug]);
+  if (!subreddit) return notFound()
 
   return (
     <div className='flex flex-col items-start gap-6'>
-      {/* ... other parts of your component ... */}
+      {/* heading /}
+      <div className='border-b border-gray-200 pb-5'>
+        <div className='-ml-2 -mt-2 flex flex-wrap items-baseline'>
+          <h3 className='ml-2 mt-2 text-base font-semibold leading-6 text-gray-900'>
+            Create Post
+          </h3>
+          <p className='ml-2 mt-1 truncate text-sm text-gray-500'>
+            in r/{params.slug}
+          </p>
+        </div>
+      </div>
 
-      {/* form */}
-      {!isLoading && <Editor subredditId={subreddit?.id} />}
+      {/ form */}
+      <Editor subredditId={subreddit.id} />
 
       <div className='w-full flex justify-end'>
-        <Button type='submit' className='w-full' form='subreddit-post-form' disabled={isLoading}>
-          {isLoading ? 'Posting...' : 'Post'}
+        <Button type='submit' className='w-full' form='subreddit-post-form'>
+          Post
         </Button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Page;
+export default page
