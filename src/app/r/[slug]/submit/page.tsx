dd52544654753f67eef43017f6e0f784924cbba2
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-10-06 01:03:43 -0700

    Update page.tsx

--- src/app/r/[slug]/submit/page.tsx
+++ src/app/r/[slug]/submit/page.tsx
@@ -19,8 +19,21 @@ const Page = ({ params }: pageProps) => {
   const isLoading = !subreddit; // Assuming 'subreddit' being null means loading
 
   return (
-    <div className='flex flex-col items-start gap-6'>
-      {/* ... other parts of your component ... */}
+      <div className='flex flex-col items-start gap-6'>
+      {/* heading /}
+      <div className='border-b border-gray-200 pb-5'>
+        <div className='-ml-2 -mt-2 flex flex-wrap items-baseline'>
+          <h3 className='ml-2 mt-2 text-base font-semibold leading-6 text-gray-900'>
+            Create Post
+          </h3>
+          <p className='ml-2 mt-1 truncate text-sm text-gray-500'>
+            in r/{params.slug}
+          </p>
+        </div>
+      </div>
+
+      {/ form */}
+      <Editor subredditId={subreddit.id} />
 
       <div className='w-full flex justify-end'>
         <Button
