a828d1b80d070c04b3202fa8e7655b5e985ae3c5
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-10-06 00:12:43 -0700

    Update page.tsx

--- src/app/r/[slug]/submit/page.tsx
+++ src/app/r/[slug]/submit/page.tsx
@@ -4,14 +4,8 @@ import { Button } from '@/components/ui/Button';
 import { db } from '@/lib/db';
 import { notFound } from 'next/navigation';
 
-interface pageProps {
-  params: {
-    slug: string;
-  };
-}
-
 const Page = ({ params }: pageProps) => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [subreddit, setSubreddit] = useState(null);
 
   useEffect(() => {
@@ -35,34 +29,19 @@ const Page = ({ params }: pageProps) => {
       }
     };
 
-    setIsLoading(true); // Set loading state to true when data fetching starts
     fetchData();
   }, [params.slug]);
 
   return (
     <div className='flex flex-col items-start gap-6'>
-      {/* heading */}
-      <div className='border-b border-gray-200 pb-5'>
-        <div className='-ml-2 -mt-2 flex flex-wrap items-baseline'>
-          <h3 className='ml-2 mt-2 text-base font-semibold leading-6 text-gray-900'>
-            Create Post
-          </h3>
-          <p className='ml-2 mt-1 truncate text-sm text-gray-500'>
-            in r/{params.slug}
-          </p>
-        </div>
-      </div>
+      {/* ... other parts of your component ... */}
 
       {/* form */}
-      {isLoading ? (
-        <p>Loading...</p> // You can replace this with a loading spinner or any loading component you prefer
-      ) : (
-        <Editor subredditId={subreddit?.id} />
-      )}
+      {!isLoading && <Editor subredditId={subreddit?.id} />}
 
       <div className='w-full flex justify-end'>
-        <Button type='submit' className='w-full' form='subreddit-post-form'>
-          Post
+        <Button type='submit' className='w-full' form='subreddit-post-form' disabled={isLoading}>
+          {isLoading ? 'Posting...' : 'Post'}
         </Button>
       </div>
     </div>
