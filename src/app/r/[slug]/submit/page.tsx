c5be2b1b4b771c13d35789089a9c749ade9b10eb
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-10-06 00:47:29 -0700

    Update page.tsx

--- src/app/r/[slug]/submit/page.tsx
+++ src/app/r/[slug]/submit/page.tsx
@@ -1,43 +1,26 @@
-import React from 'react';
-import { useMutation } from '@tanstack/react-query'
-import { Editor } from '@/components/Editor';
-import { Button } from '@/components/ui/Button';
-import { db } from '@/lib/db';
-import { notFound } from 'next/navigation';
+import { Editor } from '@/components/Editor'
+import { Button } from '@/components/ui/Button'
+import { db } from '@/lib/db'
+import { notFound } from 'next/navigation'
 
 interface pageProps {
   params: {
-    slug: string;
-  };
+    slug: string
+  }
 }
 
-const mockMutation = async () => {
-  // Simulate an asynchronous operation (e.g., API request) with a delay
-  await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulated 2-second delay
-  // Return some mock data (optional)
-  return { success: true };
-};
-
-const Page = ({ params }: pageProps) => {
-  const { mutate, isLoading } = useMutation(mockMutation);
-
-  const handleSubmit = () => {
-    mutate(); // Call the mock mutation function
-  };
-
-  const subreddit = db.subreddit.findFirst({
+const page = async ({ params }: pageProps) => {
+  const subreddit = await db.subreddit.findFirst({
     where: {
       name: params.slug,
     },
-  });
+  })
 
-  if (!subreddit) {
-    notFound();
-    return null; // Return null when subreddit is not found
-  }
+  if (!subreddit) return notFound()
 
   return (
     <div className='flex flex-col items-start gap-6'>
+      {/* heading /}
       <div className='border-b border-gray-200 pb-5'>
         <div className='-ml-2 -mt-2 flex flex-wrap items-baseline'>
           <h3 className='ml-2 mt-2 text-base font-semibold leading-6 text-gray-900'>
@@ -49,21 +32,16 @@ const Page = ({ params }: pageProps) => {
         </div>
       </div>
 
-      <Editor subredditId={subreddit?.id} />
+      {/ form */}
+      <Editor subredditId={subreddit.id} />
 
       <div className='w-full flex justify-end'>
-        <Button
-          type='submit'
-          className='w-full'
-          form='subreddit-post-form'
-          isLoading={isLoading}
-          onClick={handleSubmit}
-        >
-          Finger
+        <Button type='submit' className='w-full' form='subreddit-post-form'>
+          Post
         </Button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Page;
+export default page
