5aca0e66d7ca4aa9e329ef808c691dd3d0a27fe9
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-10-07 10:48:49 -0700

    Update SignUp.tsx

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -6,16 +6,15 @@ const SignUp = () => {
   return (
     <div className='container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]'>
       <div className='flex flex-col space-y-2 text-center'>
-        <Icons.logo className='mx-auto h-6 w-6' />
         <h1 className='text-2xl font-semibold tracking-tight'>Sign Up</h1>
         <p className='text-sm max-w-xs mx-auto'>
-          By continuing, you are setting up a Breadit account and agree to our
+          By continuing, you are setting up a Campus Connected account and agree to our
           User Agreement and Privacy Policy.
         </p>
       </div>
       <UserAuthForm />
       <p className='px-8 text-center text-sm text-muted-foreground'>
-        Already a Breadditor?{' '}
+        Already Signed Up?{' '}
         <Link
           href='/sign-in'
           className='hover:text-brand text-sm underline underline-offset-4'>
