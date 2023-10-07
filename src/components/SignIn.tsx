630c6ab8c385ea37e9dc07121afda963325a8ed5
bron-lover <111200034+bron-lover@users.noreply.github.com>
2023-10-07 10:49:39 -0700

    Update SignIn.tsx

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -6,7 +6,7 @@ const SignIn = () => {
   return (
     <div className='container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]'>
       <div className='flex flex-col space-y-2 text-center'>
-        <h1 className='text-2xl font-semibold tracking-tight'>Welcome back</h1>
+        <h1 className='text-2xl font-semibold tracking-tight'>Welcome Back</h1>
         <p className='text-sm max-w-xs mx-auto'>
           By continuing, you are setting up a Campus Connected account and agree to our
           User Agreement and Privacy Policy.
